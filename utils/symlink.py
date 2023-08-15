2cc715733e2a0f2b9b592b8e22c9e08ff7f2ce10
Cyril-Meyer <contact@cyrilmeyer.eu>
2023-08-15 14:33:27 +0200

    update symlink module to handle folder not found error

--- utils/symlink.py
+++ utils/symlink.py
@@ -2,9 +2,12 @@ import os
 
 
 def link(path, appid, mod_id):
+    if not os.path.isdir(f'{path}/steamapps/workshop/content/{appid}/{mod_id}'):
+        raise FileNotFoundError
     try:
         os.symlink(f'{path}/steamapps/workshop/content/{appid}/{mod_id}', f'{path}/@A3SH{mod_id}')
     except FileExistsError:
         pass
     except Exception as e:
         raise e
+    return f'@A3SH{mod_id}'
